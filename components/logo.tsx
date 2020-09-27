1f97eb80267952417bc05512a477a95bc62bddbb
Je <git@iamje.com>
2020-09-27 17:23:26 +0800

    chore: replace files from hello example

--- components/logo.tsx
+++ components/logo.tsx
@@ -1,7 +1,7 @@
 import React from 'https://esm.sh/react'
 
-export default function Logo({ height = 60 }: { height?: number }) {
+export default function Logo({ height = 90 }: { height?: number }) {
     return (
-        <img src="/logo.png" height={height} title="AlephJS" />
+        <img src="/logo.png" height={height} title="Aleph.js" />
     )
 }
