e73077d223ab734f314b93b215ca5df111dfc5a3
Je <git@iamje.com>
2020-10-03 11:41:15 +0800

    refactor: update app component

--- app.tsx
+++ app.tsx
@@ -1,11 +1,11 @@
-import React, { Fragment, PropsWithChildren } from 'https://esm.sh/react'
 import { Head } from 'https://deno.land/x/aleph/mod.ts'
+import React, { ComponentType } from 'https://esm.sh/react'
 import Header from './components/header.tsx'
 import './style/app.less'
 
-export default function App({ children }: PropsWithChildren<{}>) {
-     return (
-        <Fragment>
+export default function App({ Page }: { Page: ComponentType<any> }) {
+    return (
+        <>
             <Head>
                 <title>Aleph.js - The React Framework in Deno.</title>
                 <meta name="viewport" content="user-scalable=no,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0" />
@@ -13,7 +13,7 @@ export default function App({ children }: PropsWithChildren<{}>) {
                 <meta name="keywords" content="react,deno,ssr,typescript,esm,fast-refresh" />
             </Head>
             <Header />
-            {children}
-        </Fragment>
+            <Page />
+        </>
     )
 }
