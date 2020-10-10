cbaca92e1f90957d38a1e562a35ccbcf26af0ce5
Je <git@iamje.com>
2020-10-11 00:58:01 +0800

    feat:  get-stared ready

--- pages/docs.tsx
+++ pages/docs.tsx
@@ -1,11 +1,53 @@
-import { Import } from 'https://deno.land/x/aleph/mod.ts'
-import React, { ComponentType } from 'https://esm.sh/react'
+/// <reference lib="dom" />
+import { Head, Import, useRouter } from 'https://deno.land/x/aleph/mod.ts'
+import hljs from 'https://esm.sh/highlight.js/lib/core'
+import bash from 'https://esm.sh/highlight.js/lib/languages/bash'
+import javascript from 'https://esm.sh/highlight.js/lib/languages/javascript'
+import json from 'https://esm.sh/highlight.js/lib/languages/json'
+import React, { ComponentType, useEffect, useMemo } from 'https://esm.sh/react'
 
-export default function Docs({ Page }: { Page?: ComponentType<any> }) {
-   return (
-        <div className="docs-page">
+hljs.registerLanguage('javascript', javascript)
+hljs.registerLanguage('json', json)
+hljs.registerLanguage('bash', (hljs: any) => {
+    const l = bash(hljs)
+    l.keywords.built_in += ' deno aleph'
+    return l
+})
+
+interface Metadata {
+    title: string
+    author: string
+    date: string
+}
+
+export default function Docs({ Page }: { Page?: ComponentType<any> & { meta: Metadata } }) {
+    const { pagePath } = useRouter()
+    const editUrl = useMemo(() => {
+        const md = pagePath === '/docs' ? pagePath + '/index.md' : pagePath + '.md'
+        return 'https://github.com/postui/alephjs.org/edit/master/pages' + md
+    }, [pagePath])
+
+    useEffect(() => {
+        document.querySelectorAll('.docs pre > code').forEach(block => {
+            hljs.highlightBlock(block)
+        })
+    }, [Page])
+
+    return (
+        <div className="docs">
+            <Head>
+                <title>{Page?.meta.title} - Aleph.js</title>
+            </Head>
             <Import from="../style/docs.less" />
-            {Page && <Page />}
+            <aside>
+                <input placeholder="Search..." />
+            </aside>
+            <div className="content">
+                {Page && <Page />}
+                <p className="edit-link">
+                    <a href={editUrl} target="_blank">Edit this page on Github</a>
+                </p>
+            </div>
         </div>
     )
 }
--- pages/docs/get-started.md
+++ pages/docs/get-started.md
@@ -4,3 +4,58 @@ author: postUI Lab.
 date: 2020-10-01
 ---
 # Get Started
+Welcome to use **Aleph.js**!
+<br>
+Aleph.js is inspired by [next.js](https://nextjs.org), many concepts is same.
+
+## System Requirements
+You will need [deno](https://deno.land/) 1.4+ and [vscode](https://code.visualstudio.com/) with deno [extension](https://marketplace.visualstudio.com/items?itemName=denoland.vscode-deno).
+
+## Installation
+```bash
+$ deno install -A -f -n aleph https://deno.land/x/aleph/cli.ts
+```
+
+## Usage
+**Create a new app**:
+```bash
+$ aleph init hello
+$ cd hello
+```
+
+**Start the app in `development` mode**:
+```bash
+$ aleph dev
+```
+This starts the development server on http://localhost:8080.
+<br>
+Visit http://localhost:8080 to view your application.
+
+Server features:
+
+- Compile modules(`js,jsx,ts,tsx,md,css,less...`) and download deps
+- HMR With React Fast Refresh
+- Serve APIs of `./api/`
+- Server-side rendering of `./pages/`
+- Serve Static files of `./public/` (mapped to `/`)
+
+**Start the app in `production` mode**:
+```bash
+$ aleph start
+```
+The application will start at http://localhost:8080 by default.
+<br>
+The default port can be changed with `-p`.
+
+**Build the app in production mode**:
+```bash
+$ aleph build
+```
+This will export a static site in the `output` directory, you can deploy it on any servers.
+<br>
+See the [hello-world](https://aleph-hello-world.netlify.com) example on [netlify](https://netlify.com).
+
+**More usages**:
+```bash
+$ aleph -h
+```
