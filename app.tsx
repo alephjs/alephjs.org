b13a1bd1adb8a402bd98e1456df694b7cec49246
Je <git@iamje.com>
2020-10-21 01:58:50 +0800

    refactor: update app.tsx

--- app.tsx
+++ app.tsx
@@ -1,4 +1,4 @@
-import { Head, Import } from 'https://deno.land/x/aleph/mod.ts'
+import { Head, Import ,Viewport} from 'https://deno.land/x/aleph/mod.ts'
 import React, { ComponentType } from 'https://esm.sh/react'
 import Header from './components/header.tsx'
 
@@ -7,8 +7,13 @@ export default function App({ Page, pageProps }: { Page: ComponentType<any>, pag
         <>
             <Import from="./style/app.less" />
             <Head>
+                <Viewport
+                    width="device-width"
+                    initialScale={1.0}
+                    minimumScale={1.0}
+                    maximumScale={1.0}
+                />
                 <title>Aleph.js - The React Framework in Deno.</title>
-                <meta name="viewport" content="user-scalable=no,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0" />
                 <meta name="description" content="The React Framework in Deno." />
                 <meta name="keywords" content="react,deno,ssr,typescript,esm,fast-refresh" />
             </Head>
