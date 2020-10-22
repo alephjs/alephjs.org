8eabf73563de04ef7d73aa61974f55712f24a1bc
Je <git@iamje.com>
2020-10-23 02:17:40 +0800

    refactor: update design docs

--- pages/docs/design/artworks.tsx
+++ pages/docs/design/artworks.tsx
@@ -4,14 +4,16 @@ export default function Artworks() {
     return (
         <div className="artworks">
             <p><img className="medium" src="/docs/logo.svg" /></p>
-            <h3>Logo(main)</h3>
+            <h3>Logo (main)</h3>
+            <div className="large space" />
             <p><img src="/docs/logo_filled.png" /><br /><img src="/docs/logo_filled_frame.png" /></p>
-            <h3>Logo(filled)</h3>
+            <h3>Logo (filled)</h3>
+            <div className="large space" />
             <p>
                 <img className="small" src="/docs/logo_3l.png" />
                 <img className="small" src="/docs/logo_3l_round.png" />
             </p>
-            <h3>Logo(3L)</h3>
+            <h3>Logo (3L)</h3>
         </div>
     )
 }
--- pages/docs/design/theme.tsx
+++ pages/docs/design/theme.tsx
@@ -2,13 +2,19 @@ import React from 'https://esm.sh/react'
 
 export default function Theme() {
     return (
-        <div className="artworks">
-            <p><img className="round" src="/docs/theme_color.png" /></p>
+        <div className="theme">
+            <p><img className="small" src="/docs/logo.svg" /></p>
+            <div className="space" />
+            <p><img className="small" src="/docs/theme_title.svg" /></p>
+            <div className="large space" />
+            <p><img className="medium round" src="/docs/theme_color.png" /></p>
             <h3>Theme Color</h3>
-            <p><img className="round" src="/docs/theme_color_second.png" /></p>
-            <h3>Theme Color(second)</h3>
-            <p><img className="round" src="/docs/theme_color_black.png" /></p>
-            <h3>Theme Color(black)</h3>
+            <div className="space" />
+            <p><img className="medium round" src="/docs/theme_color_second.png" /></p>
+            <h3>Theme Color (second)</h3>
+            <div className="space" />
+            <p><img className="medium round" src="/docs/theme_color_black.png" /></p>
+            <h3>Theme Color (black)</h3>
         </div>
     )
 }
