39f5c443c882eaeed3a231b3795879cc37e41f54
Je <git@iamje.com>
2020-10-28 10:38:27 +0800

    refactor: use `useEnv` hook

--- app.tsx
+++ app.tsx
@@ -1,35 +1,34 @@
-import { Head, Import, Scripts, useDeno, Viewport } from 'https://deno.land/x/aleph/mod.ts'
+import { Import, Scripts, useEnv, Viewport } from 'https://deno.land/x/aleph/mod.ts'
 import React, { ComponentType } from 'https://esm.sh/react'
 import Header from './components/header.tsx'
 
 export default function App({ Page, pageProps }: { Page: ComponentType<any>, pageProps: any }) {
-    const { isDev } = useDeno(() => ({
-        isDev: (window as any).ALEPH.ENV.__buildMode === 'development'
+    const { buildMode, GTAGID } = useEnv(env => ({
+        buildMode: env.__buildMode,
+        GTAGID: env.GTAGID
     }))
 
     return (
         <>
             <Import from="./style/app.less" />
-            <Head>
-                <Viewport
-                    width="device-width"
-                    initialScale={1.0}
-                    minimumScale={1.0}
-                    maximumScale={1.0}
-                />
-            </Head>
+            <Viewport
+                width="device-width"
+                initialScale={1.0}
+                minimumScale={1.0}
+                maximumScale={1.0}
+            />
             <Header />
             <Page {...pageProps} />
-            {!isDev && (
+            {buildMode === 'production' && GTAGID && (
                 <Scripts>
-                    <script async src="https://www.googletagmanager.com/gtag/js?id=G-WDCBBBRC98"></script>
+                    <script async src={`https://www.googletagmanager.com/gtag/js?id=${encodeURIComponent(GTAGID)}`}></script>
                     <script>{`
                         window.dataLayer = window.dataLayer || [];
                         function gtag(){
                             dataLayer.push(arguments);
                         }
                         gtag('js', new Date());
-                        gtag('config', 'G-WDCBBBRC98');
+                        gtag('config', ${JSON.stringify(GTAGID)});
                     `}</script>
                 </Scripts>
             )}
@@ -37,5 +36,5 @@ export default function App({ Page, pageProps }: { Page: ComponentType<any>, pag
     )
 }
 
-// set hash anchor scroll offset to avoid the fixed header override the hash target element
-(window as any).ALEPH.hashAnchorScroll = { offset: { top: 80 } }
+// set hashAnchorScroll with offset to avoid the fixed header override the hash scroll element
+(window as any).hashAnchorScroll = { offset: { top: 80 } }
