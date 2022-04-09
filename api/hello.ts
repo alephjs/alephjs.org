ae3d04eef763547ed562131a9a902cb2b651f499
X <i@jex.me>
2022-04-09 16:14:36 +0800

    Use aleph canary

--- api/hello.ts
+++
@@ -1,5 +0,0 @@
-import type { APIHandler } from 'aleph/types.d.ts'
-
-export const handler: APIHandler = ({ response }) => {
-  response.json({ name: 'Aleph' })
-}
---
+++ server.tsx
@@ -0,0 +1,10 @@
+import { Router } from "aleph/react";
+import { serve } from "aleph/server";
+import { renderToString } from "react-dom/server";
+
+serve({
+  config: {
+    routeFiles: "./routes/**/*.tsx",
+  },
+  ssr: (ctx) => renderToString(<Router ssrContext={ctx} />),
+});
