8b28ceffe11a6a9da67a680a38f5a52cd6133971
X <i@jex.me>
2022-04-24 23:48:17 +0800

    Upgrade aleph

--- server.tsx
+++ server.tsx
@@ -1,7 +1,7 @@
 import presetUno from "@unocss/preset-uno.ts";
 import { Router } from "aleph/react";
 import { serve } from "aleph/server";
-import { renderToString } from "react-dom/server";
+import { renderToReadableStream } from "react-dom/server";
 
 serve({
   config: {
@@ -10,5 +10,8 @@ serve({
       presets: [presetUno()],
     },
   },
-  ssr: (ctx) => renderToString(<Router ssrContext={ctx} />),
+  ssr: {
+    suspense: true,
+    render: (ctx) => renderToReadableStream(<Router ssrContext={ctx} />, ctx),
+  },
 });
